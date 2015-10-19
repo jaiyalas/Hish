4cf986cafe154ec5a21a0ecc687dc71fb25bae28
jaiyalas <jaiyalas@gmail.com>
2015-10-20 02:49:12 +0800

    much modularized

--- src/Hish/SysInfo.hs
+++ src/Hish/SysInfo.hs
@@ -1,10 +1,14 @@
 module Hish.SysInfo
-  ( -- * A
+  (
+  -- * Get basic information
     uid
   , hostname
   , pwd
-  , pwdShorten
-  -- * B
+  , shortDir
+  -- * Get VCS-related information
+  , status
+  , branch
+  -- * Primitive functions
   , simpleCmd
   , argedCmd
   ) where
@@ -14,18 +18,18 @@ import System.Exit (ExitCode (..))
 import Data.List (lines,unlines)
 import Data.Char (isSpace)
 import qualified Data.String.Utils as S (split,replace,join)
-import Text.Regex.TDFA ((=~))
 
+import Hish.VCS
 
--- | Obtain username
+-- | return username
 uid :: IO (Maybe String)
-uid = simpleCmd init "whoami"
+uid = simpleCmd (Just . init) "whoami"
 
--- | Obgain hostname
+-- | return hostname
 hostname :: IO (Maybe String)
-hostname = simpleCmd init "hostname"
+hostname = argedCmd (Just . init) "hostname" ["-s"]
 
--- | Obtain current working directory
+-- | return current working directory
 pwd :: Int -- ^ threshold of shortening
     -> IO (Maybe String)
 pwd width = do
@@ -35,116 +39,67 @@ pwd width = do
     (ExitSuccess, Just name)   -> return $ return $
       ( (\str -> if (head str) == '~' then str else '/':str )
       . (\str -> if (length str) > width
-                    then pwdShorten $ S.split "/" str
+                    then shortDir $ S.split "/" str
                     else str)
       . S.replace ("/Users/"++name) "~"
       . filter (/='\n')
       ) $ out
     otherwise -> return Nothing
 
--- | Concats the given list of name into a path.
--- Notice that, this function will NOT add root '/' or home '~' directory
--- on the leftmost position. For example, `pwdShorten ["A","B","C"] = "A/B/C"`.
-pwdShorten :: [String] -- ^ a list of folder name
+-- | concating the given list of name into a path.
+-- Notice that, this function will __NOT__ add root, __/__, or home, __~__,
+-- to the leftmost position. For example,
+--
+-- >>> pwdShorten ["A","B","C"]
+-- "A/B/C"
+--
+shortDir :: [String] -- ^ a list of folder name
            -> String
-pwdShorten [] = ""
-pwdShorten [l] = l
-pwdShorten (x:xs) = (head x) : '/' : pwdShorten xs
+shortDir [] = ""
+shortDir [l] = l
+shortDir (x:xs) = (head x) : '/' : shortDir xs
 
-exeCmd :: String -> [String] -> String -> IO String
+-- | get current status.
+status :: VCS a => a        -- ^ version control system
+       -> IO (Maybe String,
+              Maybe String,
+              Maybe String) -- ^ (cleanliness, ahead, behind)
+status vcs = do
+   maybeText <- argedCmd (Just . id) "git" ["status","--porcelain","-sb"]
+   case maybeText of
+      Nothing -> return
+         ( Nothing
+         , Nothing
+         , Nothing )
+      (Just text) -> return
+         ( vcsCleanliness vcs text
+         , vcsAhead       vcs text
+         , vcsBehind      vcs text )
+
+-- | get current name of git-branch
+branch :: VCS a => a -- ^ version control system
+       -> IO (Maybe String)  -- ^ current branch name
+branch vcs =  argedCmd (vcsCurrentBranch vcs) "git" ["branch"]
+
+exeCmd :: String
+       -> [String]
+       -> String
+       -> IO (ExitCode, String, String)
 exeCmd = SP.readProcessWithExitCode
 
--- | Execute command with text handler but without args
-simpleCmd :: (String -> String) -- ^ how to handle the result
-          -> String             -- ^ command
+-- | execute command with text handler but without args
+simpleCmd :: (String -> Maybe String) -- ^ translate raw text to information
+          -> String                   -- ^ console command
           -> IO (Maybe String)
 simpleCmd handler cmd = argedCmd handler cmd []
 
--- | Execute command with text handler and args
-argedCmd :: (String -> String) -- ^ how to handle the result
-          -> String             -- ^ command
-          -> [String]           -- ^ arguments
+-- | execute command with text handler and args
+argedCmd :: (String -> Maybe String) -- ^ translate raw text to information
+          -> String                  -- ^ console command
+          -> [String]                -- ^ command arguments
           -> IO (Maybe String)
 argedCmd handler cmd args = do
    (code,stdout,_) <- exeCmd cmd args ""
    case code of
      ExitFailure _ -> return Nothing
-     ExitSuccess   -> return $ famp handler stdout
-
---
-
-
-
-
--- | Obtain current git-status.
--- Returning "#" for clean working directory.
--- Returning "*" for dirty working directory.
-status :: VCS a => a -> IO (Maybe String)
-status = argedCmd handler "git" ["status","--porcelain"] ""
-   where handler =
-      (\b->if b then "#" else "*")
-      . null
-      . map head
-      . lines
-
-{-
-$> let text = "## master...origin/master [ahead 21, behind 13]"
-$> text =~ "(ahead [0-9]+|behind [0-9]+)" :: String
-=> "ahead 21"
-
-,s =~ "behind [0-9]+" :: String
--}
-
-tracking :: VCS a => a        -- ^ version control system
-         -> IO (Maybe String,
-                Maybe String,
-                Maybe String) -- ^ (status, ahead, behind)
-tracking = argedCmd handler "git" ["status","--porcelain","-sb"] ""
-   where handler =
-      (undefined)
-      {-
-      . null
-      . filter (\s -> )
-      . S.splitWs str
-      . filter ((/='[')||(/=']')||(/=','))
-      . head
-      -}
-      . lines
---
-aheadFilter :: String -> Maybe String
-aheadFilter s =
-   case (s =~ "ahead [0-9]+"  :: String) of
-      []    -> Nothing
-      ahead -> S.replace "ahead " "" ahead
-behindFilter :: String -> Maybe Int
-behindFilter s =
-   case (s =~ "behind [0-9]+"  :: String) of
-      []    -> Nothing
-      behind -> S.replace "behind " "" behind
-
--- | Obtain current git-status.
--- Returning "#" for clean working directory.
--- Returning "*" for dirty working directory.
-git_status :: IO String
-git_status = do
-  (code,out,_) <- readProcessWithExitCode "git" ["status","--porcelain"] ""
-  case code of
-    ExitFailure _ -> return ""
-    ExitSuccess   -> return $
-      ((\b->if b then "#" else "*")
-      .null
-      .map head.lines
-      ) out
-
-
--- | Obtain current name of git-branch
-git_branch :: IO String
-git_branch = do
-  (code,out,_) <- readProcessWithExitCode "git" ["branch"] ""
-  case code of
-    ExitFailure _ -> return ""
-    ExitSuccess   -> return $
-      (drop 1
-      .head.takeWhile (('*'==).head) -- take current br
-      .lines.filter (/=' ') -- rm ' ';split by '\n'
-      ) out
+     ExitSuccess   -> return $ (Just stdout) >>= handler
--- src/Hish/VCS.hs
+++ src/Hish/VCS.hs
@@ -1,4 +1,63 @@
-module Hish.VCS () where
+module Hish.VCS
+   (
+   -- * Version Control System
+     VCS   (..)
+   -- ** Git
+   , Git   (..)
+   -- ** Darcs
+   , Darcs (..)
+   ) where
 
+import Text.Regex.TDFA ((=~))
+import qualified Data.String.Utils as S (replace)
+import qualified Data.List as DL (lines)
+
+
+-- | Every version control system provides functions as follows
+--
 class VCS a where
+   -- | tracking ahead
+   vcsAhead :: a -> String -> Maybe String
+   -- | tracking behind
+   vcsBehind :: a -> String -> Maybe String
+   -- | determining the cleanliness of working-tree
    --
+   --       * __#__ - /clean/ working directory
+   --       * __?__ - /clean/ but existing untracked file
+   --       * __*__ - /dirty/
+   vcsCleanliness :: a -> String -> Maybe String
+   vcsCurrentBranch :: a -> String -> Maybe String
+
+-- | Unit type for presenting Git.
+data Git = Git
+
+instance VCS Git where
+   vcsAhead _ s =
+      case (s =~ "ahead [0-9]+"  :: String) of
+         []    -> Nothing
+         ahead -> Just $ S.replace "ahead " "" ahead
+   vcsBehind _ s =
+      case (s =~ "behind [0-9]+"  :: String) of
+         []    -> Nothing
+         behind -> Just $ S.replace "behind " "" behind
+   vcsCleanliness _ s = let body = tail $ DL.lines s in
+      case body of
+         [] -> Just "#"
+         _  -> let (index,tree) = unzip $ map ((\ (a:b:_)->(a,b)).take 2) body in
+            if ("" == (filter (/='?') tree))
+               then Just "?"
+               else Just "*"
+   vcsCurrentBranch _ = Just
+      . drop 2
+      . head
+      . filter ((=='*').head)
+      . lines
+
+-- | Unit type for presenting Darcs.
+-- /UN-IMPLEMENTED/!
+data Darcs = Darcs
+
+instance VCS Darcs where
+   vcsAhead _ s = return "a"
+   vcsBehind _ s = return "b"
+   vcsCleanliness _ s = return "?"

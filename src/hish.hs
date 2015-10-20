77858462880e2dac7987b6d41fa8205e81e683bc
jaiyalas <jaiyalas@gmail.com>
2015-10-20 13:38:20 +0800

    generalise print functions

--- src/hish.hs
+++ src/hish.hs
@@ -1,3 +1,5 @@
+{-# OverloadedStrings #-}
+
 module Main where
 
 import Hish.ANSICode
@@ -21,46 +23,56 @@ _pwdWidth = 60
 
 main :: IO ()
 main = do
+   printUserInfo
+   printWorkingTree
+   putStr " "
+   existGit <- doesDirectoryExist ".git"
+   if existGit   then printVCSInfo VCS.Git   else return ()
+   existDarcs <- doesDirectoryExist "_darcs"
+   if existDarcs then printVCSInfo VCS.Darcs else return ()
+   putStr $ applyANSI (_prompt_symbol++" ") $ mempty
+
+--
+printUserInfo :: IO ()
+printUserInfo = do
+   _ts <- HS.time "%H:%M"
+   putStr $ applyANSI (_ts) $ fgBlackL <> mempty
+   putStr $ applyANSI "(" $ fgBlackL <> mempty
    _un <- HS.uid
    case _un of
       Nothing -> return ()
-      Just un -> putStr $ applyANSI un $ fgBlackL <> mempty
-   --
-   _hn <- HS.hostname
-   case _hn of
-      Nothing -> return ()
-      Just hn -> putStr $ applyANSI ("@"++hn) $ fgBlackL <> mempty
-   --
-   putStr " "
+      Just un -> putStr $ applyANSI (un) $ mempty
+   putStr $ applyANSI ")" $ fgBlackL <> mempty
+   -- _hn <- HS.hostname
+   -- case _hn of
+   --    Nothing -> return ()
+   --    Just hn -> putStr $ applyANSI ("@"++hn) $ fgBlackL <> mempty
+--
+printWorkingTree :: IO ()
+printWorkingTree = do
    _wd <- HS.pwd _pwdWidth
    case _wd of
       Nothing -> return ()
       Just wd -> putStr $ applyANSI wd $ fgGreen <> mempty
-   --
-   putStr " "
-   --
+--
+printVCSInfo :: (Show a, VCS.VCS a) => a -> IO ()
+printVCSInfo vcs = do
+   _br <- HS.branch vcs
+   (_st, _ah, _bh) <- HS.status vcs
+   putStr $ show vcs
    putStr "["
-   --
-   _br <- HS.branch VCS.Git
    case _br of
-      Nothing -> return ()
       Just br -> putStr $ applyANSI br $ fgWhiteL <> ESC_Bold <> mempty
-   (_st, _ah, _bh) <- HS.status VCS.Git
-   --
+      Nothing -> return ()
    case _st of
       Just "*" -> putStr $ applyANSI "*" $ ESC_Bold <> fgRed <> mempty
       Just "?" -> putStr $ applyANSI "?" $ ESC_Bold <> fgYellow <> mempty
       Just "#" -> putStr $ applyANSI "#" $ ESC_Bold <> fgGreen <> mempty
       _ -> putStr ""
-   --
    case _ah of
       Nothing -> return ()
       Just ah -> putStr $ applyANSI ("+"++ah) $ fgRedL <> mempty
-   --
-   case _ah of
+   case _bh of
       Nothing -> return ()
-      Just ah -> putStr $ applyANSI ("-"++ah) $ fgGreenL <> mempty
-   --
+      Just bh -> putStr $ applyANSI ("-"++bh) $ fgGreenL <> mempty
    putStr "]"
-   --
-   putStr $ applyANSI (_prompt_symbol++" ") $ mempty

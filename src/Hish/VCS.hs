4ed1436514b24fdf86d5cab7a6943c81168ee386
Yun-Yan Chi <jaiyalas@gmail.com>
2017-07-13 08:09:53 +0800

    adding stack-config (.cabal is not done yet)

--- src/Hish/VCS.hs
+++ src/Hish/VCS.hs
@@ -8,10 +8,12 @@ module Hish.VCS
    , Darcs (..)
    ) where
 
-import Text.Regex.TDFA ((=~))
+-- import Text.Regex.TDFA ((=~))
+import Text.Parsec
 import qualified Data.String.Utils as S (replace)
 import qualified Data.List as DL (lines)
 import System.Directory (findExecutable)
+import Data.Either
 
 -- | Every version control system provides functions as follows
 --
@@ -46,45 +48,41 @@ instance Show Git where
    show Git = "G"
 
 instance VCS Git where
-   vcsAhead _ s =
-      case (s =~ "ahead [0-9]+"  :: String) of
-         []    -> Nothing
-         ahead -> Just $ S.replace "ahead " "" ahead
-   vcsBehind _ s =
-      case (s =~ "behind [0-9]+"  :: String) of
-         []    -> Nothing
-         behind -> Just $ S.replace "behind " "" behind
-   vcsCleanliness _ s = let body = tail $ DL.lines s in
-      case body of
-         [] -> Just ""
-         _  -> let (index,tree) = unzip $ map ((\ (a:b:_)->(a,b)).take 2) body in case (filter (/=' ') index, filter (/=' ')tree) of
-            -- index == tree == "   "
-            ("","") -> Just ""
-            -- every changes are in index
-            (_,"") -> Just "#"
-            -- none change is inindex
-            ("",_) -> Just "*"
-            (idx,tre) -> if ("" == (filter (/='?') (idx++tre)))
-               -- left only untracked file
-               then Just "?"
-               -- mix situiation
-               else Just "#"
-   vcsCurrentBranch _ = Just
-      . drop 2
-      . head
-      . filter ((=='*').head)
-      . lines
+    vcsAhead _ s = either (const Nothing) Just $
+        parse (string "ahead" >> spaces >> many1 digit >>= return) "" s
+    vcsBehind _ s = either (const Nothing) Just $
+        parse (string "behind" >> spaces >> many1 digit >>= return) "" s
+    vcsCleanliness _ s = let body = tail $ DL.lines s in
+        case body of
+            [] -> Just ""
+            _  -> let (index,tree) = unzip $ map ((\ (a:b:_)->(a,b)).take 2) body in case (filter (/=' ') index, filter (/=' ')tree) of
+                -- index == tree == "   "
+                ("","") -> Just ""
+                -- every changes are in index
+                (_,"") -> Just "#"
+                -- none change is inindex
+                ("",_) -> Just "*"
+                (idx,tre) -> if ("" == (filter (/='?') (idx++tre)))
+                    -- left only untracked file
+                    then Just "?"
+                    -- mix situiation
+                    else Just "#"
+    vcsCurrentBranch _ = Just
+        . drop 2
+        . head
+        . filter ((=='*').head)
+        . lines
    --
-   branchCmd  _ = "git"
-   branchArgs _ = ["branch"]
-   statusCmd  _ = "git"
-   statusArgs _ = ["status","--porcelain","-sb"]
-   --
-   installed _ = do
-      ext <- findExecutable "git"
-      case ext of
-         Nothing -> return False
-         Just _  -> return True
+    branchCmd  _ = "git"
+    branchArgs _ = ["branch"]
+    statusCmd  _ = "git"
+    statusArgs _ = ["status","--porcelain","-sb"]
+    --
+    installed _ = do
+        ext <- findExecutable "git"
+        case ext of
+            Nothing -> return False
+            Just _  -> return True
 
 -- | Unit type for presenting Darcs.
 -- /UN-IMPLEMENTED/!
@@ -93,26 +91,26 @@ data Darcs = Darcs
 instance Show Darcs where
    show Darcs = "D"
 
-instance VCS Darcs where
-   vcsCleanliness _ ("No changes!") = Just "#"
-   vcsCleanliness _ s = let body = map head $ DL.lines s in
-      if ((filter (/='a') body)=="") then Just "?" else Just "*"
-   -- darcs is so hard to find ahead/behind
-   vcsAhead _ s = do
-      return ""
-   vcsBehind _ s = do
-      return ""
-   vcsCurrentBranch _ s =
-      -- darcs has no the concept of branch
-      return ""
-   --
-   branchCmd Darcs = "darcs"
-   branchArgs Darcs = ["whatsnew"]
-   statusCmd Darcs = "darcs"
-   statusArgs Darcs = ["whatsnew","-l"]
-   --
-   installed _ = do
-      ext <- findExecutable "darcs"
-      case ext of
-         Nothing -> return False
-         Just _  -> return True
+-- instance VCS Darcs where
+--    vcsCleanliness _ ("No changes!") = Just "#"
+--    vcsCleanliness _ s = let body = map head $ DL.lines s in
+--       if ((filter (/='a') body)=="") then Just "?" else Just "*"
+--    -- darcs is so hard to find ahead/behind
+--    vcsAhead _ s = do
+--       return ""
+--    vcsBehind _ s = do
+--       return ""
+--    vcsCurrentBranch _ s =
+--       -- darcs has no the concept of branch
+--       return ""
+--    --
+--    branchCmd Darcs = "darcs"
+--    branchArgs Darcs = ["whatsnew"]
+--    statusCmd Darcs = "darcs"
+--    statusArgs Darcs = ["whatsnew","-l"]
+--    --
+--    installed _ = do
+--       ext <- findExecutable "darcs"
+--       case ext of
+--          Nothing -> return False
+--          Just _  -> return True
